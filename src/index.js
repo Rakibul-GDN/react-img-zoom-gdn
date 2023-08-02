2ae60f3b988a9acfe439a125aac06100b1472ccd
Rakibul-GDN <rhasan.diu.cse@gmail.com>
2023-08-02 08:32:18 +0600

    some modification in code

--- src/index.js
+++ src/index.js
@@ -1,134 +1,118 @@
 /* eslint jsx-a11y/mouse-events-have-key-events: 0 */
-import React, { Component, createRef } from 'react'
-import PropTypes from 'prop-types'
-import styles from './style.css'
+import React, { Component, createRef } from "react";
+import PropTypes from "prop-types";
+import styles from "./style.css";
 
 class Zoom extends Component {
-  constructor (props) {
-    super(props)
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      zoom: false,
-      mouseX: null,
-      mouseY: null,
+        this.state = {
+            zoom: false,
+            mouseX: null,
+            mouseY: null,
+        };
+
+        const { height, img, transitionTime, width } = props;
+
+        this.outerDivStyle = {
+            height: `${height}px`,
+            width: `${width}px`,
+            overflow: "hidden",
+        };
+
+        this.innerDivStyle = {
+            height: `${height}px`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "auto 100%",
+            transition: `transform ${transitionTime}s ease-out`,
+            backgroundImage: `url('${img}')`,
+        };
+
+        this.imageRef = createRef();
+
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+        this.handleMouseMovement = this.handleMouseMovement.bind(this);
     }
 
-    const {
-      height,
-      img,
-      transitionTime,
-      width,
-    } = props
-
-    this.outerDivStyle = {
-      height: `${height}px`,
-      width: `${width}px`,
-      overflow: 'hidden',
+    handleMouseOver() {
+        this.setState({
+            zoom: true,
+        });
     }
 
-    this.innerDivStyle = {
-      height: `${height}px`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundSize: 'auto 100%',
-      transition: `transform ${transitionTime}s ease-out`,
-      backgroundImage: `url('${img}')`,
+    handleMouseOut() {
+        this.setState({
+            zoom: false,
+        });
     }
 
-    this.imageRef = createRef()
+    handleMouseMovement(e) {
+        const { left: offsetLeft, top: offsetTop } =
+            this.imageRef.current.getBoundingClientRect();
 
-    this.handleMouseOver = this.handleMouseOver.bind(this)
-    this.handleMouseOut = this.handleMouseOut.bind(this)
-    this.handleMouseMovement = this.handleMouseMovement.bind(this)
-  }
+        const {
+            current: {
+                style: { height, width },
+            },
+        } = this.imageRef;
 
-  handleMouseOver () {
-    this.setState({
-      zoom: true,
-    })
-  }
+        const x = ((e.screenX - offsetLeft) / parseInt(width, 10)) * 60;
+        const y = ((e.screenY - offsetTop) / parseInt(height, 10)) * 100;
 
-  handleMouseOut () {
-    this.setState({
-      zoom: false,
-    })
-  }
-
-  handleMouseMovement (e) {
-    const {
-      left: offsetLeft,
-      top: offsetTop,
-    } = this.imageRef.current.getBoundingClientRect()
-
-    const {
-      current: {
-        style: {
-          height,
-          width,
-        },
-      },
-    } = this.imageRef
-
-    const x = ((e.pageX - offsetLeft) / parseInt(width, 10)) * 100
-    const y = ((e.pageY - offsetTop) / parseInt(height, 10)) * 100
-
-    this.setState({
-      mouseX: x,
-      mouseY: y,
-    })
-  }
-
-  render () {
-    const {
-      mouseX,
-      mouseY,
-      zoom,
-    } = this.state
-
-    const {
-      zoomScale,
-    } = this.props
-
-    const transform = {
-      transformOrigin: `${mouseX}% ${mouseY}%`,
+        this.setState({
+            mouseX: x,
+            mouseY: y,
+        });
     }
 
-    return (
-      <div
-        style={this.outerDivStyle}
-        onMouseOver={this.handleMouseOver}
-        onMouseOut={this.handleMouseOut}
-        onMouseMove={this.handleMouseMovement}
-        ref={this.imageRef}
-      >
-        <div
-          style={{
-            ...transform,
-            ...this.innerDivStyle,
-            transform: zoom ? `scale(${zoomScale})` : 'scale(1.0)',
-          }}
-          className={styles.zoomImg}
-        />
-      </div>
-    )
-  }
+    render() {
+        const { mouseX, mouseY, zoom } = this.state;
+
+        const { zoomScale } = this.props;
+
+        const transform = {
+            transformOrigin: `${mouseX}% ${mouseY}%`,
+        };
+
+        return (
+            <div
+                style={this.outerDivStyle}
+                onMouseOver={this.handleMouseOver}
+                onMouseOut={this.handleMouseOut}
+                onMouseMove={this.handleMouseMovement}
+                ref={this.imageRef}
+            >
+                <div
+                    style={{
+                        ...transform,
+                        ...this.innerDivStyle,
+                        transform: zoom ? `scale(${zoomScale})` : "scale(1.0)",
+                    }}
+                    className={styles.zoomImg}
+                />
+            </div>
+        );
+    }
 }
 
 Zoom.propTypes = {
-  /** The path to the image. It can be a url. */
-  img: PropTypes.string.isRequired,
-  /** The zoom scale. */
-  zoomScale: PropTypes.number.isRequired,
-  /** The height of the image in pixels */
-  height: PropTypes.number.isRequired,
-  /** The width of the image in pixels */
-  width: PropTypes.number.isRequired,
-  /** The time (in seconds) that will take to scale your image. */
-  transitionTime: PropTypes.number,
-}
+    /** The path to the image. It can be a url. */
+    img: PropTypes.string.isRequired,
+    /** The zoom scale. */
+    zoomScale: PropTypes.number.isRequired,
+    /** The height of the image in pixels */
+    height: PropTypes.number.isRequired,
+    /** The width of the image in pixels */
+    width: PropTypes.number.isRequired,
+    /** The time (in seconds) that will take to scale your image. */
+    transitionTime: PropTypes.number,
+};
 
 Zoom.defaultProps = {
-  transitionTime: 0.1,
-}
+    transitionTime: 0.1,
+};
 
-export default Zoom
+export default Zoom;
